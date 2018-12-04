327a003b344c7e1d828e1683c48e7974a4a638ae
Paul-Cl-ark <clark.paul123@gmail.com>
2018-12-04 20:26:39 +0000

    added first booking validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,13 @@ class Booking < ApplicationRecord
   has_one :studio, through: :room
   # has_many :photo_equipment_bookings
 
+  validates :date, :time_slot, presence: true
+  validate :future_date
+
+  def future_date
+    if date.present? && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
+
 end
